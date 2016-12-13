828d6edc14a7c73096e4a8a7b1076ae881112188
needle <xznarz@gmail.com>
2016-12-13 21:23:31 +0200

    Update package JSON (add expect and deepFreeze library) Create stateless component Counter Update eslint rules

--- app/index.js
+++ app/index.js
@@ -1,12 +1,15 @@
+import React, { PropTypes } from 'react';
+import ReactDom from 'react-dom';
+import expect, { createSpy, spyOn, isSpy } from 'expect';
+import deepFreeze from 'deep-freeze';
 import { createStore } from 'redux';
-
 
 const counter = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 2;
+      return state + 1;
     case 'DECREMENT':
-      return state - 2;
+      return state - 1;
     default:
       return state;
   }
@@ -14,17 +17,34 @@ const counter = (state = 0, action) => {
 
 const store = createStore(counter);
 
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement,
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
 const render = () => {
-  document.body.innerText = store.getState();
+  ReactDom.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    document.getElementById('root'),
+  );
 };
 
 store.subscribe(render);
 render();
 
-document.addEventListener('click', () => {
-  store.dispatch({ type: 'INCREMENT' });
-});
-
-document.addEventListener('contextmenu', () => {
-  store.dispatch({ type: 'DECREMENT' });
-});
+Counter.propTypes = {
+  value: PropTypes.number.isRequired,
+  onIncrement: PropTypes.func.isRequired,
+  onDecrement: PropTypes.func.isRequired,
+};
