c7280cb1b53db9dec829a1e07b365abaf607d00b
needle <xznarz@gmail.com>
2016-12-20 21:18:44 +0200

    prepare file structure for the app

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import { Provider, connect } from 'react-redux';
 
 ReactDom.render(
   <Provider store={createStore(store, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+    <Shop />
   </Provider>,
   document.getElementById('root'),
 );
