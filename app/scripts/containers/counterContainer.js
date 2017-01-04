a299313a40340526772f27fce5962cd52fe748c5
Denys Yefremov <denysYefremov@users.noreply.github.com>
2017-01-04 16:53:06 +0200

    fix counterContainer.js

--- app/scripts/containers/counterContainer.js
+++ app/scripts/containers/counterContainer.js
@@ -2,15 +2,8 @@ import { connect } from 'react-redux';
 import Counter from '../atoms/counter';
 import { getItemsCountInBasket } from '../selectors/selectors';
 
-
-const mapStateToProps = state => ({
-  ...state,
-});
-
-const mapDispatchToProps = dispatch => ({});
-
 const margeProps = state => ({
   number: getItemsCountInBasket(state),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps, margeProps)(Counter);
+export default connect(null, null, margeProps)(Counter);
