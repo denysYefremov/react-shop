b9ddbb8cb1988d0e81cf9377ccf0b9a080d34500
Denys Yefremov <denysYefremov@users.noreply.github.com>
2017-01-04 17:08:11 +0200

    Update counterContainer.js

--- app/scripts/containers/counterContainer.js
+++ app/scripts/containers/counterContainer.js
@@ -2,8 +2,10 @@ import { connect } from 'react-redux';
 import Counter from '../atoms/counter';
 import { getItemsCountInBasket } from '../selectors/selectors';
 
+const mapStateToProps = (state) => state;
+
 const margeProps = state => ({
   number: getItemsCountInBasket(state),
 });
 
-export default connect(null, null, margeProps)(Counter);
+export default connect(mapStateToProps, null, margeProps)(Counter);
