a5abdac2d5fcb7c290bf74d1e9f613765081e2ea
needle <xznarz@gmail.com>
2017-01-07 00:43:02 +0200

    add router

--- app/scripts/containers/productPageContainer.js
+++ app/scripts/containers/productPageContainer.js
@@ -4,8 +4,8 @@ import { getItems, getItemById } from '../selectors/selectors';
 
 const mapStateToProps = state => ({ products: getItems(state) });
 
-const margeProps = ({ products }, dispatchProps, { params }) => ({
-  product: getItemById(products, parseInt(params.itemId, 0)),
+const margeProps = ({ products }, dispatchProps, { params: { itemId } }) => ({
+  product: getItemById(products, parseInt(itemId, 0)),
 });
 
 export default connect(mapStateToProps, null, margeProps)(ProductPage);
