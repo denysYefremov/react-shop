8741bab65309055209dc245ccf6c87e1c561e4b7
needle <xznarz@gmail.com>
2017-01-09 12:03:42 +0200

    add search products

--- app/scripts/molecules/search.js
+++ app/scripts/molecules/search.js
@@ -1,11 +1,7 @@
 import React, { PropTypes } from 'react';
 import { filterItems } from '../actions/productActions';
 
-const Search = ({ dispatch }) => (
-  <div>
-    <input type="text" onChange={e => dispatch(filterItems(e.target.value))} />
-  </div>
-);
+const Search = ({ dispatch }) => <input type="text" onChange={e => dispatch(filterItems(e.target.value))} />;
 
 Search.propTypes = {
   dispatch: PropTypes.func.isRequired,
